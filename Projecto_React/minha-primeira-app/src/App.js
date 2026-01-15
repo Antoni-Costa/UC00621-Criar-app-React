eb39f01c6935491d21981f1872490c7fa0928348
Antoni-Costa <210736270+Antoni-Costa@users.noreply.github.com>
2026-01-15 19:47:58 +0000

    aula 1 - Criando meu primeiro componente em React

--- Projecto_React/minha-primeira-app/src/App.js
+++ Projecto_React/minha-primeira-app/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import Componente1 from "./Componente1";
 
 function App() {
+
   return (
     <div className="App">
- 
+      <Componente1 nome="Tony" idade={25} />
     </div>
   );
 }
---
+++ Projecto_React/minha-primeira-app/src/Componente1.jsx
@@ -0,0 +1,10 @@
+function Componente1({ nome, idade }) {
+
+    return (
+    <div className="App">
+      <h1>Hello {nome} tu tens {idade} anos</h1>
+    </div>
+  );
+}
+
+export default Componente1;
