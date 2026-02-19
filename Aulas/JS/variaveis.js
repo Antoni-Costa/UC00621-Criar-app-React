cfc43a601fdac94a5f2e343bb82d489b4533662d
Antoni-Costa <210736270+Antoni-Costa@users.noreply.github.com>
2026-02-19 16:46:14 +0000

    ,

---
+++ Aulas/JS/variaveis.js
@@ -0,0 +1,15 @@
+// Declaração e inicialização de variáveis
+
+//string
+let nome = "Maria";
+console.log("Nome:", nome);
+console.log(typeof nome);
+
+
+// number
+let idade = 25;
+let altura = 1.65;
+console.log("Idade:", idade);
+console.log("Altura:", altura);
+console.log(typeof idade);
+console.log(typeof altura);
